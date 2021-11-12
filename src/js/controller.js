dc814ecde689973336404426dee3bf3cad0a460b
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2021-11-12 09:27:21 +0100

    Completely finished pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,7 +5,7 @@ import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
-
+import paginationView from "./views/paginationView.js";
 
 // Polyfilling
 import 'core-js/stable';
@@ -17,25 +17,25 @@ const controlRecipes = async function () {
   try {
     // Take search hash
     const id = window.location.hash.slice(1)
-    
+
     // Guard clause if no id found from hash 
     if (!id) return;
 
     // 1. Start loading the spinner when we fetch data 
     recipeView.renderLoader()
-    
+
     // 1. Loading the recipe
     await model.loadRecipe(id);
 
     // 2. Rendering the recipe 
     recipeView.render(model.state.recipe)
-    
+
   } catch (e) {
     recipeView.renderError();
   }
 };
 
-const controlSearchResults = async function() {
+const controlSearchResults = async function () {
   try {
     resultsView.renderLoader()
 
@@ -45,19 +45,31 @@ const controlSearchResults = async function() {
 
     // Load search results
     await model.loadSearchResults(query)
-    
-    // Render results
+
+    // Get results of certain page
     //resultsView.render(model.state.search.results)
-    resultsView.render(model.getSearchResultsPage(1))
+    resultsView.render(model.getSearchResultsPage())
+
+    // Render the results
+    paginationView.render(model.state.search)
   } catch (e) {
     console.log(e);
   }
 }
 
+const controlPagination = function (goToPage) {
+  // Get NEW results of certain page
+  resultsView.render(model.getSearchResultsPage(goToPage))
+  // Render the results
+  paginationView.render(model.state.search)
+
+}
+
 // Subscriber
-const init = function() {
+const init = function () {
   recipeView.addHandlerRender(controlRecipes)
   searchView.addHandlerSearch(controlSearchResults)
+  paginationView.addHandlerClick(controlPagination)
 }
 
 init();
