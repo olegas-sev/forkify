a379ee263dde11f7586cb2d4b2520073c3c76cf4
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2021-11-19 10:35:43 +0100

    docJSing and gitignore update

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,7 +3,16 @@ import icons from 'url:../../img/icons.svg'
 export default class View {
     _data;
 
-
+    /**
+     * 
+     * @param {Object | Object[]} data The data to be rendered (e.g recipe)
+     * @param {boolean} [render=true] If false create markup string instead of rendering to the DOM 
+     * @returns {undefined | string} A markup string is returned if render=false
+     * @this {Object} View instance
+     * @author Olegas Sevcenko
+     * @todo Finish implementation
+     * @returns 
+     */
     render(data, render = true) {
         if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
         
